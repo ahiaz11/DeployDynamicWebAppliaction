cd209da940ea53de0b2d7198044325ce51065e5b
ahiaz11 <ahiaz11@gmail.com>
2024-06-27 15:36:54 +0300

    update

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "value"
-    key            = "value"
-    region         = "value"
-    profile        = "value" 
-    dynamodb_table = "value"
-  }
-}

327df24704df3c17c82ea7e0dba1a61699c69b59
ahiaz demri <ahiaz.demri@scanovate.com>
2024-06-30 13:56:44 +0300

    add and update

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "value"
+    key            = "value"
+    region         = "value"
+    profile        = "value" 
+    dynamodb_table = "value"
+  }
+}
